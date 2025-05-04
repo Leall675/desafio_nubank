5c3ae6641b61f625caa603405afbb1a5225a4aca
Matheus Leal Da Silva <maldsilva@rd.com.br>
2025-05-04 20:35:39 -0300

    feat: Criacao de busca parametrizada atraves do cpf

--- src/main/java/spring/boot/desafio/desafio_nubank/controller/ClientesController.java
+++ src/main/java/spring/boot/desafio/desafio_nubank/controller/ClientesController.java
@@ -1,5 +1,6 @@
 package spring.boot.desafio.desafio_nubank.controller;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import spring.boot.desafio.desafio_nubank.dto.ClientesDto;
 import spring.boot.desafio.desafio_nubank.dto.ClientesResponseDto;
 import spring.boot.desafio.desafio_nubank.dto.ContatoResponseDto;
-import spring.boot.desafio.desafio_nubank.dto.ContatosDto;
 import spring.boot.desafio.desafio_nubank.model.Clientes;
 import spring.boot.desafio.desafio_nubank.service.ClientesService;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,35 +24,27 @@ public class ClientesController {
 
     @PostMapping
     public ResponseEntity<ClientesResponseDto> salvarClientes(@RequestBody ClientesDto dto) {
-        System.out.println("DTO Recebido no Controlador: " + dto);
-        // Verifique os contatos
-        for (ContatosDto contato : dto.getContatos()) {
-            System.out.println("Contato: telefone=" + contato.getTelefone() + ", email=" + contato.getEmail());
-        }
-
         Clientes clienteSalvo = clientesService.salvarCliente(dto);
-
         List<ContatoResponseDto> contatoResponseDtos = clienteSalvo.getContatos().stream()
                 .map(contato -> new ContatoResponseDto(contato.getId(), contato.getTelefone(), contato.getEmail(), clienteSalvo.getId()))
                 .collect(Collectors.toList());
-
         ClientesResponseDto responseDto = new ClientesResponseDto(clienteSalvo.getId(), clienteSalvo.getName(), clienteSalvo.getCpf(), contatoResponseDtos);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
     @GetMapping
-    public ResponseEntity<List<ClientesResponseDto>> listarClientes() {
-        List<ClientesResponseDto> clientes = clientesService.listarTodos();
-        if (clientes.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    public ResponseEntity<List<ClientesResponseDto>> listarClientes(@RequestParam(required = false) String cpf) {
+        if (cpf != null && !cpf.isEmpty()) {
+            ClientesResponseDto cliente = clientesService.buscarClientePorCpf(cpf);
+            return ResponseEntity.ok(Collections.singletonList(cliente));
         }
-        return ResponseEntity.ok(clientesService.listarTodos());
+        List<ClientesResponseDto> clientes = clientesService.listarTodos();
+        return ResponseEntity.ok(clientes);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ClientesResponseDto> buscarClientePorId(@PathVariable Long id) {
-        ClientesResponseDto clientes = (ClientesResponseDto) clientesService.buscarClientePorID(id);
+        ClientesResponseDto clientes = clientesService.buscarClientePorID(id);
         return ResponseEntity.ok(clientes);
     }
 
