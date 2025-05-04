11adc8f2d1c2158748e6ef100a46b4bcac967bba
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-05-04 20:40:18 -0300

    fix: ajuste de rota

--- src/main/java/spring/boot/desafio/desafio_nubank/controller/ClientesController.java
+++ src/main/java/spring/boot/desafio/desafio_nubank/controller/ClientesController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("api/v1/cliente")
+@RequestMapping("api/v1/clientes")
 public class ClientesController {
 
     @Autowired
